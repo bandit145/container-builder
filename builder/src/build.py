a610d30fdc9bae66d22b36189c8110eb67dc0aaf
Philip Bove <pbove@redhat.com>
2021-02-12 15:46:21 -0500

    added test for tags

--- builder/src/build.py
+++ builder/src/build.py
@@ -66,19 +66,17 @@ class Build:
                 self.push(cont, repo)
 
     def get_repo_tags(self, repo):
-        repo_domain, repo = repo.split("/")
+        repo = repo.split("/")
+        repo_domain = repo[0]
+        del repo[0] 
         repo = "/".join(repo)
         req = requests.get(f"https://{repo_domain}/v2/{repo}/tags/list")
         # deal with betas etc.
         # add in  master/main/latest support
+        self.logger.debug(f"Tags from repo {req.json()['tags']}")
         tags = sorted(
-            [
-                semver.VersionInfo.parse(x.strip("v"))
-                for x in req.json()["tags"]
-                if x != "latest"
-            ],
-            reverse=True,
-        )
+            [semver.VersionInfo.parse(x.strip("v")) for x in req.json()["tags"] if x != "latest"],
+            reverse=True)
         return tags
 
     def test(self, tag, capabilities, tests):
--- tests/integration/test_build.py
+++ tests/integration/test_build.py
@@ -30,3 +30,8 @@ def test_test():
     img = build.build(conf["repo"] + conf["tag"], "doh")
     build.test(conf["repo"] + conf["tag"], None, tests)
     os.chdir(cwd)
+
+
+def test_tags():
+	build = Build(logging, "TEST_USER_NAME", "TEST_USER_PASS")
+	assert '2.6.0' in build.get_repo_tags('quay.io/prometheus/prometheus')
