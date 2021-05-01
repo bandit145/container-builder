5fcbae19a8d4230f8a15e0c0f5844521447039a7
Philip Bove <phil@bove.online>
2021-04-30 20:34:47 -0400

    rc5 release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt", "r") as reqs:
 
 setup(
     name="container-builder",
-    version="1.0.0-rc.4",
+    version="1.0.0-rc.5",
     description="manage container builds in a single repo",
     author="Philip Bove",
     install_requires=requirements,
